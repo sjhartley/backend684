830934ee56b6352918fdb8cf17d46a63511eac3e
sjhartley <89655207+sjhartley@users.noreply.github.com>
2022-05-03 19:53:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ function tag_remover(str){
 
 //NYSE API
 
-function nyse_get(keyWord, res){
+function nyse_get(params, res){
+
+  console.log("params...");
+  console.log(params);
 
   var url="https://www.nyse.com/api/quotes/filter";
   var payload={"instrumentType":"EQUITY","pageNumber":1,"sortColumn":"NORMALIZED_TICKER","sortOrder":"ASC","maxResultsPerPage":10,"filterToken":""};
